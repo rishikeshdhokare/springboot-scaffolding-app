4c3690f30b061292b6be902c46af3e3843e56c29
rishikeshdhokare <rishikesh.m.dhokare@gmail.com>
2020-06-01 20:15:06 +0200

    initial commit

---
+++ src/main/java/com/rishikeshdhokare/springbootscaffoldingapp/SpringbootScaffoldingAppApplication.java
@@ -0,0 +1,13 @@
+package com.rishikeshdhokare.springbootscaffoldingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScaffoldingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScaffoldingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishikeshdhokare/springbootscaffoldingapp/SpringbootScaffoldingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishikeshdhokare.springbootscaffoldingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootScaffoldingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
